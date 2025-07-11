fd31ba77e1d6768d2ecf1b67d21af1086a148a9e
teena1313 <tinalai.34@gmail.com>
2025-07-10 22:39:11 -0700

    for aws

--- src/webpack.config2.js
+++ src/webpack.config2.js
@@ -13,7 +13,7 @@ const info = {
 const myrouter = "http://76.138.149.125:8088";
 // const myrouter = "https://notecard-bcknd-chdmgkeuhahdczh7.westus-01.azurewebsites.net";
 // const mytarget = "https://proud-bush-0ea3e931e.2.azurestaticapps.net:8080";
-const mytarget = "https://notecard-ftend-faa6dna6dnetgwa6.westus-01.azurewebsites.net"
+const mytarget = "https://amplify.d2hdfqxipjw4to.amplifyapp.com/"
 
 const config = {
   mode: 'development',
